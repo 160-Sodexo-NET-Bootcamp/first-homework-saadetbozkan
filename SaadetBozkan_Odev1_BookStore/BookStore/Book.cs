be9f33c02ef0f0fb72ac3364d171d91aa5a530dc
saadetbozkan <saadetbozkan@gmail.com>
2021-12-22 02:19:25 +0300

    added BookStore Project

---
+++ SaadetBozkan_Odev1_BookStore/BookStore/Book.cs
@@ -0,0 +1,12 @@
+namespace BookStore
+{
+    public class Book
+       
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int SerialNumber { get; set; }
+    }
+}
